2d8ce7f9bf31b291e7a28fc1a08b3476b2b7f38c
DOHYUN <pastapeter@naver.com>
2021-08-01 00:12:16 +0900

    match된것 변경 비동기처리

---
+++ Set_StanfordCS193/Set_StanfordCS193/ViewController.swift
@@ -0,0 +1,140 @@
+//
+//  ViewController.swift
+//  Set_StanfordCS193
+//
+//  Created by abc on 2021/07/31.
+//
+
+// todo:
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var setCards = Cards() // 전체 덱
+    
+    @IBOutlet private var setButtons: [UIButton]!
+    @IBOutlet private var startButton: UIButton!
+    @IBOutlet private var moreCardButton: UIButton!
+    
+    var cardCollection = [Card]()
+    
+    private var selectedButtonNum: Int {
+        get {
+            var count = 0
+            setButtons.forEach { (button) in
+                if (button.isSelected) {
+                    count += 1
+                }
+            }
+            return count
+            }
+        }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        for button in setButtons {
+            if let card = setCards.draw() {
+                cardCollection.append(card)
+                setButtonUI(with: button, with: card)
+            }
+        }
+        NotificationCenter.default.addObserver(self, selector: #selector(updateButtonSelection(_:)), name: Notification.Name("doItAgainNotification"), object: nil)
+        setCards.delegate = self
+    }
+    
+    func setButtonUI(with button: UIButton, with card: Card) {
+        let buttonString = String([Character](repeating: Character(card.cardShape.rawValue), count: card.cardNumber))
+        var attributes = [NSAttributedString.Key: Any]()
+        switch card.cardShade {
+            case .empty:
+                attributes = [
+                    .strokeColor : card.cardColor.create ,
+                    .strokeWidth : -5.0
+                ]
+                let attributeString = NSAttributedString(string: buttonString, attributes: attributes)
+                button.setAttributedTitle(attributeString, for: .normal)
+            case .fill:
+                attributes = [
+                    .foregroundColor: card.cardColor.create.withAlphaComponent(1)]
+                let attributeString = NSAttributedString(string: buttonString, attributes: attributes)
+                button.setAttributedTitle(attributeString, for: .normal)
+            case .shade:
+                attributes = [
+                    .foregroundColor : card.cardColor.create.withAlphaComponent(0.15),
+                ]
+                let attributeString = NSAttributedString(string: buttonString, attributes: attributes)
+                button.setAttributedTitle(attributeString, for: .normal)
+            }
+        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+    
+    @objc private func updateButtonSelection(_ notification: Notification){
+        setButtons.forEach { (button) in
+            button.isSelected = false
+            button.isEnabled = true
+        }
+    }
+    
+    @IBAction private func moreCardButtonDidTap(_ sender: UIButton) {
+        for _ in 0..<3 {
+            if let card = setCards.draw() {
+                print(card)
+            }
+        }
+    }
+    
+    @IBAction func cardButtonDidTap(_ sender: UIButton) {
+        sender.isSelected = true
+        guard let cardIndex = setButtons.firstIndex(of: sender) else {
+            print("buttonIndex Error")
+            return
+        }
+        sender.isEnabled = false
+        setCards.chooseCards(at: cardIndex)
+        updateButtons()
+    }
+    
+    private func updateButtons() {
+        print("selectedButtnoNUm \(selectedButtonNum)")
+        if selectedButtonNum > 2 {
+            setButtons.filter {!$0.isSelected}.forEach { (button) in
+                button.isEnabled = true
+            }
+        } else {
+            setButtons.forEach { (button) in
+                if !button.isSelected {
+                    button.isEnabled = true
+                }
+            }
+        }
+        
+    }
+
+
+}
+
+
+extension ViewController: isMatchedDelegate {
+    
+    func setCardDidMatched(_ Cards: Cards, indexs: [Int]) {
+        var newCards = [Card]()
+        var newCardIndex = 0
+        DispatchQueue.main.async {
+            for _ in 0..<3 {
+                if let card = self.setCards.draw() {
+                    newCards.append(card)
+                }
+            }
+            for index in indexs {
+                self.cardCollection[index] = newCards[newCardIndex]
+                newCardIndex += 1
+            }
+            for buttonIndex in self.setButtons.indices {
+                self.setButtonUI(with: self.setButtons[buttonIndex], with: self.cardCollection[buttonIndex])
+            }
+        }
+    }
+}
